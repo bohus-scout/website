ec2840d196c874f9c2543c829b0ec48d8ac5113d
Emil Öberg <obergsgbg-dev@yahoo.se>
2018-03-23 22:21:37 +0100

    preparing for deployment of anmalan
    auto-deploy script and a makefile for copying changed files

---
+++ auto-deploy.mk
@@ -0,0 +1,11 @@
+# script that will only uppdate changed sources in anmalan
+FROM=anmalan-google-form
+TO=${HOME}/public_html/scout
+
+SRC=$(notdir $(wildcard ${FROM}/*))
+
+
+all: ${SRC:%=${TO}/%}
+${SRC:%=${TO}/%}: ${TO}/% : ${FROM}/%
+        cp ${FROM}/$* ${TO}/$*
+
